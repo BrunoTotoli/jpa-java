26a9ab72e42a20b8ff197b1a3fb4e48ca3d07958
Bruno Totoli <santosbruno101062@gmail.com>
2022-05-04 22:56:32 -0300

    Insert database

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,15 +2,27 @@ package application;
 
 import model.entities.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Program {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa(null, "Joao da Pedra", "joaodapedra@gmail.com");
         Pessoa p2 = new Pessoa(null, "Carlo da Pedra", "carlodapedra@gmail.com");
         Pessoa p3 = new Pessoa(null, "Jaime da Pedra", "jaimedapedra@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Pronto");
 
     }
+
+
 }
